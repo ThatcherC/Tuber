e64894c7a259b8d49c724bf74f360c62e8b13601
Thatcher Chamberlin <j.thatcher.c@gmail.com>
2016-09-17 14:43:23 -0400

    Switched to a node.js server

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-//server.js
+//server.js for tuber
 
 var express = require('express');
 var bodyParser = require('body-parser');
 var fs = require('fs');
-var exec = require('child_process').exec;
-var config = require('./config');
 
 var app = express();
+app.use(express.static('static'));
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
@@ -14,4 +13,9 @@ app.listen(8080);
 
 app.post("/",function(req, res){
   console.log(req.body);
+});
+
+http://stackoverflow.com/questions/4529586/render-basic-html-view-in-node-js-express
+app.get("/",function(req,res){
+	res.render('main');
 });
