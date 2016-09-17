52a00ec3054e1a9e67d55ad14fb3448c545a491b
Thatcher Chamberlin <j.thatcher.c@gmail.com>
2016-09-17 13:24:21 -0400

    Got the map working

--- tuber.js
+++ tuber.js
@@ -1,1 +1,10 @@
 //tuber js file
+
+var map;
+
+function initializeMap(){
+  map = new google.maps.Map(document.getElementById('gmap'), {
+          center: {lat: 42.359667, lng: -71.087549},
+          zoom: 13
+        });
+}
