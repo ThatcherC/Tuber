86fd44af756fc5a69097d41a56a2fa21732f9c7d
Teddy Laurita <Teddy Laurita>
2016-09-18 01:21:19 -0400

    worked on refactoring modes to be scalable

--- modes.js
+++ modes.js
@@ -1,26 +1,71 @@
 /* modes.js */
 var apis = require("./apis");
+var request = require('request');
 
 /* Mode info */
-var baseModeInitInfo = {
-  "Cartwheeling":{
-    "shortName":"cartwheeling",
-    "baseMode":"walking",
-    "scaleFactors":{
-      "baseMode":"walkingObject",
-      "time":5.4,
-      "energy":12.5,
-      "stylepoints":21
-    }
+var baseModeInit = {
+  "Cartwheeling": {
+    "shortName": "cartwheeling",
+    "baseMode": "walking",
+    "baseModeObj": "walkingObject",
+    "time": 5.4,
+    "energy": 12.5,
+    "stylepoints": 21
   },
   "Hoverboarding- Back to the Future Style":{
-    
+    "shortName": "hoverboarding1",
+    "baseMode": "biking",
+    "baseModeObj": "bikingObject",
+    "time": 0.7,
+    "energy": 0.1,
+    "stylepoints": .65
+  },
+  "Leapfrogging": {
+    "shortName": "leapfrogging",
+    "baseMode": "walking",
+    "baseModeObj": "walkingObject",
+    "time": 5.9,
+    "energy": 8.1,
+    "stylepoints": .54
+  },
+  "Wheelchair": {
+    "shortName": "wheelchair",
+    "baseMode": "walking",
+    "baseModeObj": "walkingObject",
+    "time": 2.9,
+    "energy": 0,
+    "stylepoints": .12
+  },
+  "Skateboarding": {
+    "shortName": "skateboarding",
+    "baseMode": "walking",
+    "baseModeObj": "walkingObject",
+    "time": 1.7,
+    "energy": 2.6,
+    "stylepoints": .15
+  },
+  "Golfcarting": {
+    "shortName": "golfcarting",
+    "baseMode": "biking",
+    "baseModeObj": "bikingObject",
+    "time": 1.7,
+    "energy": 2.6,
+    "stylepoints": .15
   }
-}
+};
 
+var otherModeInit = {
+  "Airplane": {
+    "shortName": "airplane",
+    "baseMode": "other",
+    "baseModeObj": "walkingObject",
+    "speed": 250,
+    "energy": 0,
+    "stylepoints": 1
+  }
+};
 
 var modeList = {};
-var request = require('request');
 
 //Creates a new mode with a name, a base mode (walking, driving, or other),
 //and a function to modify the base mode.
@@ -32,66 +77,39 @@ function addMode(shortName, displayName, baseMode, eval){
   modeList[shortName] = obj;
 }
 
+var keys = Object.keys(baseModeInitInfo);
+var i = 0
+for (i; i < keys.length; i++) {
+  var modeToAdd = baseModeInit[keys[i]]
+  addMode(modeToAdd["shortName"],
+          keys[i],
+          modeToAdd["baseMode"],
+          function(modeToAdd["baseModeObj"]) {
+            var object = {};
+            object.time = modeToAdd["time"];
+            object.energy = modeToAdd["energy"];
+            object.stylepoints = modeToAdd["stylepoints"];
+            return object;
+          });
+}
 
-addMode("cartwheeling", "Cartwheeling", 'walking', function(walkingObject){
-  var cartwheelingObject = {};
-  cartwheelingObject.time = walkingObject.time*5.4;
-  cartwheelingObject.energy = walkingObject.energy*12.5;
-  cartwheelingObject.stylepoints = walkingObject.stylepoints*.21;
-  return cartwheelingObject;
-});
-
-addMode("hoverboarding1","Hoverboarding- Back to the Future Style", 'biking', function(bikingObject){
-  var hoverboardingObject = {};
-  hoverboardingObject.time = bikingObject.time*0.7;
-  hoverboardingObject.energy = bikingObject.energy*0.1;
-  hoverboardingObject.stylepoints = bikingObject.stylepoints*.65;
-  return hoverboardingObject;
-});
-
-addMode("leapfrogging", "Leapfrogging", 'walking', function(walkingObject){
-	var leapfroggingObject = {};
-  leapfroggingObject.time = walkingObject.time*5.9;
-  leapfroggingObject.energy = walkingObject.energy*8.1;
-  leapfroggingObject.stylepoints = walkingObject.stylepoints*.54;
-  return leapfroggingObject;
-});
-
-addMode("wheelchair","Wheelchair", 'walking', function(walkingObject){
-	var wheelchairObject = {};
-  wheelchairObject.time = walkingObject.time*2.9;
-  wheelchairObject.energy = walkingObject.energy*0;
-  walkingObject.stylepoints = walkingObject.stylepoints*.12;
-  return wheelchairObject;
-});
-
-addMode("skateboarding","Skateboarding", 'walking', function(bikingObject){
-  var skateboardingObject = {};
-  skateboardingObject.time = bikingObject.time*1.7;
-  skateboardingObject.energy = bikingObject.energy*2.6;
-  skateboardingObject.stylepoints = bikingObject.stylepoints*.15;
-  return skateboardingObject;
-});
-
-addMode("golfcarting", "Golfcarting", 'biking', function(bikingObject){
-  var golfcartingObject = {};
-  golfcartingObject.time = bikingObject.time*1.7;
-  golfcartingObject.energy = bikingObject.energy*2.6;
-  golfcartingObject.stylepoints = bikingObject.stylepoints*.15;
-  return golfcartingObject;
-});
-
-addMode("airplane", "Airplane", 'other', function(walkingObject){
-  var airplaneObject = {};
-  var speed = 250;
-  console.log(walkingObject)
-  var directDistance = calcStraightDist(walkingObject.start_coords,walkingObject.end_coords);
-
-  airplaneObject.time = directDistance/speed;
-  airplaneObject.energy = 0;
-  airplaneObject.stylepoints = 1;/*depends on the type of plane*/
-  return airplaneObject;
-});
+keys = Object.keys(otherModeInit);
+for (i = 0; i < keys.length; i++) {
+  var modeToAdd = otherModeInit[keys[i]]
+  addMode(modeToAdd["shortName"],
+          keys[i],
+          modeToAdd["baseMode"],
+          function(modeToAdd["baseModeObj"]){
+            var object = {};
+            var directDistance = calcStraightDist(modeToAdd["baseModeObj"].start_coords,
+                                                  modeToAdd["baseModeObj"].end_coords);
+            object.time = directDistance/modeToAdd["speed"];
+            object.time = modeToAdd["time"];
+            object.energy = modeToAdd["energy"];
+            object.stylepoints = modeToAdd["stylepoints"];
+            return object;
+          });
+}
 
 function calcStraightDist(start_coords, end_coords) {
   var R = 6371e3; // metres
