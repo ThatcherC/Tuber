8ee3a63e228f05636bc904ff091c4f8d883c38db
Thatcher Chamberlin <j.thatcher.c@gmail.com>
2016-09-17 20:34:21 -0400

    Added marker drag detection

--- static/tuber.js
+++ static/tuber.js
@@ -10,7 +10,7 @@ var destinationBox;
 function initializeMap(){
   map = new google.maps.Map(document.getElementById('gmap'), {
           center: {lat: 42.359667, lng: -71.087549},
-          zoom: 13
+          zoom: 14
         });
   startBox = document.getElementById('startLocation');
   endBox   = document.getElementById('endLocation');
@@ -25,15 +25,25 @@ function setStart(){
     label: "Start Point",
     draggable: true
   });
-  startBox.value = center.lat+", "+center.lng;
+  startBox.value = center.lat().toFixed(5)+", "+center.lng().toFixed(5);
+  startMarker.addListener('dragend',function(){
+    var p = startMarker.getPosition();
+    startBox.value = p.lat().toFixed(5)+", "+p.lng().toFixed(5);
+  });
 }
 
 function setDestination(){
   console.log("Setting end point");
+  var center = map.getCenter();
   endMarker = new google.maps.Marker({
-    position: map.getCenter(),
+    position: center,
     map: map,
     label: "End Point",
     draggable: true
+  });
+  endBox.value = center.lat().toFixed(5)+", "+center.lng().toFixed(5);
+  endMarker.addListener('dragend',function(){
+    var p = endMarker.getPosition();
+    endBox.value = p.lat().toFixed(5)+", "+p.lng().toFixed(5);
   });
 }
